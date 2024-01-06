ea15d6a87c2790f9388a61ce8363f8453b1f45dd
Kadomoni <105569628+kadomoni@users.noreply.github.com>
2024-01-06 09:11:48 -0500

    Fixed non-shop display

--- jellyneo.userscript.js
+++ jellyneo.userscript.js
@@ -1,17 +1,24 @@
 // ==UserScript==
-// @name     JellyNeo Assistant
-// @author   Kadomoni
-// @version  1
-// @grant    none
-// @match    https://*.neopets.com/*
-// @require  https://code.jquery.com/jquery-3.6.4.min.js
+// @name JellyNeo Assistant
+// @author Kadomoni
+// @version 2
+// @grant none
+// @match https://*.neopets.com/*
+// @require https://code.jquery.com/jquery-3.6.4.min.js
+// @downloadURL https://raw.githubusercontent.com/kadomoni/JellyNeo-Assistant/main/jellyneo.userscript.js
 // ==/UserScript==
 const sleep = async (ms) => {return new Promise(resolve => setTimeout(resolve, ms*100));}
 
 function displayValue(item) {
   $.get("https://items.jellyneo.net/search/?name=" + item.innerText.trim() + "&name_type=3&limit=1", (res)=>{
     let price = $(res).find('.price-history-link').text();
-    if(price){ $(item).next().next().append("<br /><font color='green'>Jelly: " + price + "</font>"); }
+    if(price){
+      if( window.location.href.includes("type=shop")){
+      	$(item).next().next().append("<br /><font color='green'>Jelly: " + price + "</font>"); 
+      } else {
+      	$(item).append("<br /><font color='green'>Jelly: " + price + "</font>"); 
+      }
+    }
   });
 }
 
@@ -23,6 +30,7 @@ function displayValue(item) {
     await sleep(30);
     
     let items = $(".item-name");
+    console.log(items);
     
     if (items.length > 0) {
       
